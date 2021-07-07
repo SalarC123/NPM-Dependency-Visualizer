ab90a1dd18b83addd56fc6c92c6b7aeb82ccf9dd
SalarC123 <salarwcheema@gmail.com>
2021-07-06 22:08:58 -0500

    create-react-app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+
+    return (
+        <div className="App">
+            <h1 style={{fontSize:"5rem", fontWeight: "bold"}}>
+                NPM Dependency Visualizer
+			</h1>
+        </div>
+    );
+}
+
+export default App;
